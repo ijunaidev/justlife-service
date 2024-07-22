e3e3af257f35a318fabaee81905391155997bc95
Junaid Tariq <ijunaidev@gmail.com>
2024-07-22 23:27:36 +0500

    Initial commit

---
+++ src/main/java/com/justlife/JustlifeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.justlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustlifeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustlifeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/justlife/JustlifeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustlifeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
